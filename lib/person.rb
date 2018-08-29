061ed19d529c2ffdd0ff73e813de64c25b4f7c60
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-08-29 00:08:40 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,6 @@
 class Person
   
-  def initialize(first_name, last_name)
-    first_name = @first_name
-    last_name = @last_name
-  end
+
   
   def name=(full_name)
     first_name, last_name = full_name.split
